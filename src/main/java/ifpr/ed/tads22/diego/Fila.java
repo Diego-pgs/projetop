7525544b6d934eeaea10163becdd2237999abf1d
Diego Pontes <ediego912@gmail.com>
2023-05-31 20:01:10 -0300

    Fila em java

---
+++ src/main/java/ifpr/ed/tads22/diego/Fila.java
@@ -0,0 +1,39 @@
+package ifpr.ed.tads22.diego;
+
+public class Fila {
+    private Nodo head;
+    private Nodo tail;
+    private Nodo novo;
+
+    public Nodo getHead(){ // retorna começo cabeça
+        return head;
+    }
+
+    public Nodo getTail(){ // retorna fim calda
+        return tail;
+    }
+
+    public void enfileirar(int i){  //ENQUEUE, Insere um elemento ao final da fila
+        novo = new Nodo();
+        novo.setValue(i);
+
+        if(head == null){
+            head=novo;
+            tail=novo;
+        }else{
+            tail.setProx(novo);
+            tail = novo;
+        }
+    }
+
+    public void desenfileira(){ //DEQUEUE, Remove um elemento do início da fila
+        head = head.getProx();
+    }
+
+    public void print(Nodo n){
+        System.out.printf(n.getValue()+" ");
+        if(n.getProx() != null){
+            print(n.getProx());
+        }
+    }
+}
