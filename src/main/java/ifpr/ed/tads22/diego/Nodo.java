ed9aaacbfa738bf23a92fa987f2a2533b095227d
Diego Pontes <ediego912@gmail.com>
2023-05-31 19:59:08 -0300

    cria metodos do nodo

--- src/main/java/ifpr/ed/tads22/diego/Nodo.java
+++ src/main/java/ifpr/ed/tads22/diego/Nodo.java
@@ -3,20 +3,31 @@ package ifpr.ed.tads22.diego;
 public class Nodo {
     private int value;
     private Nodo prox;
+    private char code;
+    private boolean ehBoolean;
 
-    public int getValue(){
-        return value;
+    public char getCode() {
+        return code;
     }
-    
-    public Nodo getProx(){
-        return prox;
+    public int getValue() {
+        return this.value;
     }
-
-    public void setValue(int value){
+    public Nodo getProx() {
+        return this.prox;
+    }
+    public void setCode(char code) {
+        this.code = code;
+    }
+    public void setBoolean(boolean ehBoolean) {
+        this.ehBoolean = ehBoolean;
+    }
+    public void setValue(int value) {
         this.value = value;
     }
-    
-    public void setProx(Nodo prox){
+    public void setProx(Nodo prox) {
         this.prox = prox;
+    }
+    public boolean isBoolean() {
+        return ehBoolean;
     }
 }
